246e85ed26a0765d0c1985bfd75aafbaffd50535
Thomas Eldridge <t.eldridge@pgr.reading.ac.uk>
2017-04-13 14:55:03 +0100

    some attribute names (and data types) added for Data object. some (corresponding to data) missing - require decision on best way to proceed.

--- TAP_reader/Data.py
+++ TAP_reader/Data.py
@@ -1,6 +1,27 @@
+import datetime as dt
+import numpy as np
+
 class Data:
     def __init__(self):
-        # define all class attributes
+    # define all class attributes
+        # shared attributes:
+        self.record_number = list()
+        self.record_id = list()
+        # 10 (header) attributes:
+        self.file_number = int(0)
+        self.orbit_number = int(0)
+        self.start_dt = dt.datetime(1978,01,1)
+        self.stop_dt = dt.datetime(1978,01,01)
+        self.south_dt = dt.datetime(1978,01,01)
+        self.north_dt = dt.datetime(1978,01,01)
+        self.descent_lon = float(0)
+        self.ascent_lon = float(0)
+        self.anode_dt = dt.datetime(1978,01,01)
+        self.solar_dec = float(0)
+        self.vapour_table = np.zeros((256),float)
+        self.window_table = np.zeros((256),float)
+        # 11 (data) attributes:
+
     # methods:
     #   include methods for adding to attributes via the
     #       - treat_as_header
