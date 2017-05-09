f7bacfcc58d702f08d529b00ed428c74f3df2728
Thomas Eldridge <t.eldridge@pgr.reading.ac.uk>
2017-05-09 15:33:38 +0100

    commit of main to work alongside consolidated Data

--- TAP_reader/main.py
+++ TAP_reader/main.py
@@ -6,13 +6,7 @@ def read_TAP_file(filename):
         - filename; a string corresponding to the complete path to a
           Nimbus 7 TAP file.
     Opens the file in read binary mode. Reads the file and writes it as a NetCDF."""
-    f = open(filename, 'rb') # open te file in read binary mode
-    np.fromfile(f, dtype=np.int32, count=1) # skips first word - all first words in all files are identical and extraneous
-    end = False
-    File = Data()
-    while not end:
-        block = scan_block(f, n_words=2324) # unit test MUST make sure that this n_words is appropriate for all cases
-        end = treat_block(File, block, False)
+    File = Data(filename)
     write_nc(File)
 
 if __name__=='__main__':
